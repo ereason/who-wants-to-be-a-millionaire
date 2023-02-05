4e6ef577a032cd930dcc3da191557453249ed8c4
george <epifantsev.george@gmail.com>
2023-02-05 19:00:48 +0700

    Created empty proj

---
+++ who-wants-to-be-a-millionaire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  who-wants-to-be-a-millionaire
+//
+//  Created by George on 05.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
